a92401fee9ed7e1e904c77589a1c6ad67a0bfaab
071568307 <071568307@109R0310BJYL6V1.EDU.YRDSB.NET>
2016-09-27 14:45:34 -0400

    Merge origin/master
    Conflicts:
    	TheCalculatza16/src/thecalculatza16/Scientific.java

--- TheCalculatza16/src/thecalculatza16/Scientific.java
+++ TheCalculatza16/src/thecalculatza16/Scientific.java
@@ -6,39 +6,43 @@ package thecalculatza16;
  */
 public class Scientific {
 
-    public static double volOfRect(double length, double width, double height) {
+    public static double volOfCylc(double radius, double height) {
+        return (height * (4 / 3) * Math.PI * (Math.pow(radius, 3)));
+    }
+
+    public static double volOfRectPrism(double length, double width, double height) {
         return (length * width * height);
     }
-    
+
     public static double volOfSphere(double radius) {
-        return ((4/3)*Math.PI*(Math.pow(radius, 3)));
+        return ((4 / 3) * Math.PI * (Math.pow(radius, 3)));
     }
-    
+
     public static double volOfPyramid(double length, double width, double height) {
-        return ((length * width * height)/3);
+        return ((length * width * height) / 3);
     }
-    
+
     public static double areaOfRect(double l, double w) {
-        return (l*w);
+        return (l * w);
     }
-    
+
     public static double areaOfRectPrism(double l, double w, double h) {
-        return (2*l*w + 2*l*h + 2*w*h);
+        return (2 * l * w + 2 * l * h + 2 * w * h);
     }
-    
+
     public static double areaOfCircle(double r) {
         return (Math.PI * r * r);
     }
-    
+
     public static double areaOfSphere(double r) {
         return (4 * Math.PI * r * r);
     }
-    
+
     public static double areaOfTriangle(double b, double h) {
         return (b * h / 2);
     }
-    
+
     public static double areaOfTrapeze(double b, double a, double h) {
-        return ((a+b)*h/2);
+        return ((a + b) * h / 2);
     }
 }
