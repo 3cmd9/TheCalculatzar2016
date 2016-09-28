024fc3bb5e6770fe4fed64154e140d8acf22fd32
073685844 <073685844@109R03100320B1B.EDU.YRDSB.NET>
2016-09-28 14:32:09 -0400

    why not?

--- TheCalculatza16/src/com/snj/BusinessFeelsSparkly.java
+++ TheCalculatza16/src/com/snj/BusinessFeelsSparkly.java
@@ -16,7 +16,7 @@ public class BusinessFeelsSparkly {
      // System.out.println (simple(55000,3, 10));  
         
         //System.out.println(mortgage(100000, 0.005, 12, 15));
-        
+        //
         
         
         
